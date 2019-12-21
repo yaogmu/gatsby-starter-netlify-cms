812a22eaafc90822656026701f1aa8ed8b18a80d
omuth <omuth@vrm.de>
2019-12-21 10:41:25 +0100

    commented out

--- src/templates/verein-page.js
+++ src/templates/verein-page.js
@@ -159,89 +159,89 @@ VereinPage.propTypes = {
 export default VereinPage
 
 export const VereinPageQuery = graphql`
-  query VereinPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      frontmatter {
-        title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
-        main {
-          heading
-          description
-          image1 {
-            alt
-            image {
-              childImageSharp {
-                fluid(maxWidth: 526, quality: 92) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-          image2 {
-            alt
-            image {
-              childImageSharp {
-                fluid(maxWidth: 526, quality: 92) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-          image3 {
-            alt
-            image {
-              childImageSharp {
-                fluid(maxWidth: 1075, quality: 72) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
-        testimonials {
-          author
-          quote
-        }
-        full_image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        pricing {
-          heading
-          description
-          plans {
-            description
-            items
-            plan
-            price
-          }
-        }
-      }
-    }
-  }
+  // query VereinPage($id: String!) {
+  //   markdownRemark(id: { eq: $id }) {
+  //     frontmatter {
+  //       title
+  //       image {
+  //         childImageSharp {
+  //           fluid(maxWidth: 2048, quality: 100) {
+  //             ...GatsbyImageSharpFluid
+  //           }
+  //         }
+  //       }
+  //       heading
+  //       description
+  //       intro {
+  //         blurbs {
+  //           image {
+  //             childImageSharp {
+  //               fluid(maxWidth: 240, quality: 64) {
+  //                 ...GatsbyImageSharpFluid
+  //               }
+  //             }
+  //           }
+  //           text
+  //         }
+  //         heading
+  //         description
+  //       }
+  //       main {
+  //         heading
+  //         description
+  //         image1 {
+  //           alt
+  //           image {
+  //             childImageSharp {
+  //               fluid(maxWidth: 526, quality: 92) {
+  //                 ...GatsbyImageSharpFluid
+  //               }
+  //             }
+  //           }
+  //         }
+  //         image2 {
+  //           alt
+  //           image {
+  //             childImageSharp {
+  //               fluid(maxWidth: 526, quality: 92) {
+  //                 ...GatsbyImageSharpFluid
+  //               }
+  //             }
+  //           }
+  //         }
+  //         image3 {
+  //           alt
+  //           image {
+  //             childImageSharp {
+  //               fluid(maxWidth: 1075, quality: 72) {
+  //                 ...GatsbyImageSharpFluid
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //       testimonials {
+  //         author
+  //         quote
+  //       }
+  //       full_image {
+  //         childImageSharp {
+  //           fluid(maxWidth: 2048, quality: 100) {
+  //             ...GatsbyImageSharpFluid
+  //           }
+  //         }
+  //       }
+  //       pricing {
+  //         heading
+  //         description
+  //         plans {
+  //           description
+  //           items
+  //           plan
+  //           price
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 `
